5a17ea7be04241bf56b999dd2586dd3855992876
SubhasriNatarajan <subinatraj@gmail.com>
2019-06-14 06:37:27 -0700

    Execute a Maven project using another Maven project dependency

---
+++ Project2/src/main/java/Booking_Details.java
@@ -0,0 +1,13 @@
+import project1.Train;
+
+public class Booking_Details 
+{
+	 public static void main(String[] args)
+	   {
+			Train tr=new Train("sabari","covai","hyderabad",3425l,8.10,8.45,7.00);
+			tr.Train_Details();
+			
+			
+
+		}
+}
